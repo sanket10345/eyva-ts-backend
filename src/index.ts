63e668ead5dd30de478944b3f86c71458c38f66d
sanket <mail.sanket10345@gmail.com>
2024-06-11 10:59:24 +0530

    Init Commit with Json

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import  memberRoutes from './routes/members'; // Adjust the path to where your members routes are located
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+const corsOptions = {
+  origin: '*', // Allow only this origin
+  methods: 'GET,POST,PUT,DELETE',  // Allow only these methods
+  allowedHeaders: 'Content-Type,Authorization', // Allow only these headers
+};
+
+// Use cors middleware with options
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.use('/api',memberRoutes); // Use the router as middleware under the `/api` path
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
